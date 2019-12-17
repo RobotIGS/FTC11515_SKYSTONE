b2f06673ae768ab52616fe88f53cc323f724e96a
Malte <malte.juenemann@igs-goettingen.de>
2019-12-17 20:12:20 +0100

    Degree methode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library/GeneralTools.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library/GeneralTools.java
@@ -3,17 +3,25 @@ package org.firstinspires.ftc.teamcode.Library;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassis;
+import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareGyro;
 
 public class GeneralTools {
     HardwareMap hardwareMap;
     HardwareChassis robot;
 
+    HardwareGyro gyro;
+
     private LinearOpMode opMode;
     public GeneralTools(LinearOpMode opMode, HardwareChassis robot) {
         this.opMode = opMode;
-
         this.robot = robot;
+
+        this.gyro = new HardwareGyro(this.opMode.hardwareMap);
+        this.gyro.init(this.opMode.hardwareMap);
     }
 
 
@@ -36,6 +44,11 @@ public class GeneralTools {
 
     public void openClamp () {
         robot.servo_2.setPosition(0.6);
+    }
+
+    //get current degrees of robot
+    public double getDegree(){
+        return 180 + gyro.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle; //hier first vllt zu second/third
     }
 }
 
